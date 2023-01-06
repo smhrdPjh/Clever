ec18c99013ec3e511f5772bcadc84f6c9a365732
smhrdPjh <pjh01082037858@gmail.com>
2023-01-06 12:23:37 +0900

    backend commit

---
+++ back_end/clever/src/main/java/com/clever/CleverApplication.java
@@ -0,0 +1,13 @@
+package com.clever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleverApplication.class, args);
+	}
+
+}
---
+++ back_end/clever/src/test/java/com/clever/CleverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
